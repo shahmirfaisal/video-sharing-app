d36acac5f704f86b198fd16eb421e42bcfa3fb67
Shahmir <shahmir049@gmail.com>
2020-05-03 14:00:45 -0700

    Profile Page Added

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import classes from "./Profile.module.css";
+import { useDocumentTitle } from "../../hooks/useDocumentTitle";
+import imgSrc from "../../assets/profile-img.jpg";
+import { Button } from "../../styled-components/Button";
+import { Videos } from "../../components/Videos/Videos";
 
 export const Profile = () => {
-  return <section>Profile</section>;
+  useDocumentTitle("Web Dev Simplified");
+
+  return (
+    <section>
+      <header className={classes.header}>
+        <div className={classes.img}>
+          <img src={imgSrc} alt="Profile Image" />
+        </div>
+
+        <div className={classes.info}>
+          <h3>Web Dev Simplified</h3>
+          <p>145k subscribers</p>
+        </div>
+
+        <Button>SUBSCRIBE</Button>
+      </header>
+
+      <h2 className={classes.heading}>Videos</h2>
+
+      <Videos />
+    </section>
+  );
 };
--- frontend/src/styled-components/Button.jsx
+++ frontend/src/styled-components/Button.jsx
@@ -3,13 +3,23 @@ import styled, { css } from "styled-components";
 export const Button = styled.button`
   border: none;
   outline: none;
-  background-image: linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%);
+
+  ${(props) =>
+    props.backgroundColor && props.backgroundColor.length > 0
+      ? css`
+          background-color: ${props.backgroundColor};
+        `
+      : css`
+          background-image: linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%);
+        `}
+
   color: white;
   padding: 0.7rem 1.5rem;
   border-radius: 3px;
   cursor: pointer;
   box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2);
   font-size: 1rem;
+
   ${(props) =>
     props.block
       ? css`
