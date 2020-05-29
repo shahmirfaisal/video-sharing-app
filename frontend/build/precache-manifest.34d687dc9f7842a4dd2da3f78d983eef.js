ced9e453374e99a6be603026c3f68cd3ca59ac3e
Shahmir <shahmir049@gmail.com>
2020-05-29 02:43:05 -0700

    Build

---
+++ frontend/build/precache-manifest.34d687dc9f7842a4dd2da3f78d983eef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be03f38eb4a9c5dbe44aa4261fbb63ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5210e7c9abffea7c30c",
+    "url": "/static/css/2.f5389bce.chunk.css"
+  },
+  {
+    "revision": "ba9f85a4198bc29a5eba",
+    "url": "/static/css/main.f43eabda.chunk.css"
+  },
+  {
+    "revision": "e5210e7c9abffea7c30c",
+    "url": "/static/js/2.73f4acfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.73f4acfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9f85a4198bc29a5eba",
+    "url": "/static/js/main.ffcc0ec6.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
